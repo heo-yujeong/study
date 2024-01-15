163fe18effec7e0fcbb55fabc7a857ed6104a392
heo-yujeong <yu.jeong5899@gmail.com>
2024-01-15 17:51:45 +0900

    24.01.15 수업

--- 01_python/python01/python_ws_1_4/ws_1_4.py
+++ 01_python/python01/python_ws_1_4/ws_1_4.py
@@ -3,15 +3,15 @@
 # 반지름
 15
 
-circum = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
-rad = 15
+PI = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
+radius = 15
 
-circumference = '원주율 : '
-radius = '반지름 : '
-round = '원의 둘레 : '
-area = '원의 넓이 : '
+PI_is = '원주율 : '
+radius_is = '반지름 : '
+round_is = '원의 둘레 : '
+area_is = '원의 넓이 : '
 
-print(f'{circumference} {circum}')
-print(f'{radius} {rad}')
-print(f'{round} {rad * 2 * circum}')
-print(f'{area} {rad * rad * circum}')
+print(f'{PI_is}{PI}')
+print(f'{radius_is}{radius}')
+print(f'{round_is}{radius * 2 * PI}')
+print(f'{area_is}{radius * radius * PI}')
