55caa8b3dd3e02d3ade2d4303d5d527e626176d4
heo-yujeong <yu.jeong5899@gmail.com>
2024-01-15 22:00:29 +0900

    24.01.15 수업 복습

---
+++ 01_python/python02/python_ws_2_a/ws_2_a.py
@@ -0,0 +1,12 @@
+# 아래에 코드를 작성하시오.
+zero_list = [0]
+print(zero_list)
+
+many_zero_list = [0] * 250000
+print(len(many_zero_list))
+
+
+numbers = [i+1 for i in range(10)]
+print(numbers)
+
+print(numbers[3:])
