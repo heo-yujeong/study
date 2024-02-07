00469247aeaf7546e8fb67c70099e48432c776d6
heo-yujeong <yu.jeong5899@gmail.com>
2024-02-07 21:55:29 +0900

    24.02.07 복습

--- 02_algorithm/99_Extra/2w_Extra/5432_쇠막대기_자르기/solution.py
+++ 02_algorithm/99_Extra/2w_Extra/5432_쇠막대기_자르기/solution.py
@@ -4,10 +4,20 @@ sys.stdin = open('input.txt')
 T = int(input())
 
 for test_case in range(1, T+1):
-    laser = input()
-    result = 0
+    laser_bar = input()
 
+    result = 0 # 잘린 개수
+    bar = 0 # 쇠막대 수
 
+    for i in range(len(laser_bar)):
+        if laser_bar[i] == '(':
+            bar += 1
+        else:
+            bar -= 1
 
+            if laser_bar[i-1] == '(' : # 레이저 였다면
+                result += bar
+            else:
+                result += 1
 
     print(f'#{test_case} {result}')
