f1122d145bb67aa4d554245a9f267ba655097498
heo-yujeong <yu.jeong5899@gmail.com>
2024-02-15 20:18:52 +0900

    24.02.15 복습

--- 02_algorithm/IM/SWEA/1970_쉬운거스름돈/sol.py
+++ 02_algorithm/IM/SWEA/1970_쉬운거스름돈/sol.py
@@ -8,17 +8,10 @@ for test_case in range(1, T+1):
     money_count = [0] * 8
     money_unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
 
-    check = False
-
-    while total_money > 10:
-        for unit in money_unit:
+    for unit in money_unit:
+        if total_money >= unit:
             money_count[money_unit.index(unit)] = total_money // unit
             total_money = total_money % unit
-            if total_money == 0:
-                check = True
-                break
-        if check:
-            break
 
     print(f'#{test_case}')
     print(*money_count)
