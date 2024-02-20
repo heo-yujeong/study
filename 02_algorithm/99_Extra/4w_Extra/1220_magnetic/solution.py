98a70aafabf1dd07f842beed47ba0e119899c9b0
heo-yujeong <yu.jeong5899@gmail.com>
2024-02-21 01:39:20 +0900

    24.02.20 복습

--- 02_algorithm/99_Extra/4w_Extra/1220_magnetic/solution.py
+++ 02_algorithm/99_Extra/4w_Extra/1220_magnetic/solution.py
@@ -1,3 +1,18 @@
 import sys
 sys.stdin = open('input.txt')
 
+for test_case in range(1, 11):
+    N = int(input())
+    table = [list(map(int, input().split())) for _ in range(N)]
+
+    count = 0
+    for j in range(N):
+        n_plus = []
+        for i in range(N):
+            if table[i][j] == 1:
+                n_plus.append(1)
+            elif table[i][j] == 2 and n_plus:
+                count += 1
+                n_plus = []
+
+    print(f'#{test_case} {count}')
