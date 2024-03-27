cfbfe648da6de622bc70e0d4818595f2c68df5bb
heo-yujeong <yu.jeong5899@gmail.com>
2024-03-27 23:19:42 +0900

    24.03.27 복습

---
+++ 05_django/06_Django_Form/django_ws_6_4/menus/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from .models import Menu
+
+
+class MenuForm(forms.ModelForm):
+
+    class Meta:
+        model = Menu
+        fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': '메뉴 이름을 작성해 주세요.'
+                }
+            ),
+            'price': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': '메뉴 설명을 작성해 주세요.'
+                }
+            ),
+            'is_vegan': forms.CheckboxInput(
+                attrs={
+                    'class': 'form-check-input'
+                }
+            )
+        }
