41834da968f5108a1852bfd4e21a3b28e4dad183
heo-yujeong <yu.jeong5899@gmail.com>
2024-03-28 17:26:24 +0900

    24.03.28 수업

---
+++ 05_django/07_Static_files/django_ws_7_4/articles/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Article
+
+
+class ArticleForm(forms.ModelForm):
+
+    class Meta:
+        model = Article
+        fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(
+                attrs={
+                    'class':'form-control',
+                }
+            ),
+            'content': forms.TextInput(
+                attrs={
+                    'class':'form-control',
+                }
+            ),
+            'image_description': forms.Textarea(
+                attrs={
+                    'class':'form-control',
+                }
+            ),
+            'is_public': forms.CheckboxInput(
+                attrs={
+                    'class': 'form-check-input'
+                }
+            )
+        }
